ce31397c30ccac16d24b30eef8ded869d4c2438b
Sutanrrier <pabloryanbr0@gmail.com>
2023-03-25 14:21:46 -0300

    Refatorando + sons pelo teclado adicionado

--- 07 - Javascript - Drum Kit/index.js
+++ 07 - Javascript - Drum Kit/index.js
@@ -1,33 +1,44 @@
+//Keysound pelo clique
 document.querySelectorAll("button").forEach(function (botao) {
     botao.addEventListener("click", function () {
-        let audio;
-
-        switch (this.innerHTML) {
-            case "w":
-                audio = new Audio("./sounds/tom-1.mp3");
-                break;
-            case "a":
-                audio = new Audio("./sounds/tom-2.mp3");
-                break;
-            case "s":
-                audio = new Audio("./sounds/tom-3.mp3");
-                break;
-            case "d":
-                audio = new Audio("./sounds/tom-4.mp3");
-                break;
-            case "j":
-                audio = new Audio("./sounds/snare.mp3");
-                break;
-            case "k":
-                audio = new Audio("./sounds/crash.mp3");
-                break;
-            case "l":
-                audio = new Audio("./sounds/kick-bass.mp3");
-                break;
-            default:
-                console.log("No key pressed.")
-        }
-
-        audio.play();
+        getKeySound(this.innerHTML);
     });
 });
+
+//Keysound pelo teclado
+document.addEventListener("keydown", function (event) {
+    getKeySound(event.key);
+});
+
+//Lógica para Keysound
+function getKeySound(botao) {
+    let audio;
+
+    switch (botao) {
+        case "w":
+            audio = new Audio("./sounds/tom-1.mp3");
+            break;
+        case "a":
+            audio = new Audio("./sounds/tom-2.mp3");
+            break;
+        case "s":
+            audio = new Audio("./sounds/tom-3.mp3");
+            break;
+        case "d":
+            audio = new Audio("./sounds/tom-4.mp3");
+            break;
+        case "j":
+            audio = new Audio("./sounds/snare.mp3");
+            break;
+        case "k":
+            audio = new Audio("./sounds/crash.mp3");
+            break;
+        case "l":
+            audio = new Audio("./sounds/kick-bass.mp3");
+            break;
+    }
+
+    if (audio != undefined) {
+        audio.play();
+    }
+}
