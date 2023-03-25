7f3711a4c0710e57f390736bae92575e835b6980
Sutanrrier <pabloryanbr0@gmail.com>
2023-03-24 22:43:53 -0300

    Projeto 06 adicionado - Dicee Game

---
+++ 06 - Javascript - The Dicee Game/index.js
@@ -0,0 +1,15 @@
+const randomNumber1 = Math.floor(Math.random() * 6) + 1;
+const randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNumber1 + ".png");
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "👍 Jogador 1 ganhou!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Jogador 2 ganhou! 👍";
+}
+else {
+    document.querySelector("h1").innerHTML = "Empate!";
+}
