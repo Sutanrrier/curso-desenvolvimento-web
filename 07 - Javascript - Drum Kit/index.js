370248d69d1a80e1657c7c805e59e938529c7284
Sutanrrier <pabloryanbr0@gmail.com>
2023-03-25 11:47:56 -0300

    Projeto 07 iniciado - Drum Kit

---
+++ 07 - Javascript - Drum Kit/index.js
@@ -0,0 +1,33 @@
+document.querySelectorAll("button").forEach(function (botao) {
+    botao.addEventListener("click", function () {
+        let audio;
+
+        switch (this.innerHTML) {
+            case "w":
+                audio = new Audio("./sounds/tom-1.mp3");
+                break;
+            case "a":
+                audio = new Audio("./sounds/tom-2.mp3");
+                break;
+            case "s":
+                audio = new Audio("./sounds/tom-3.mp3");
+                break;
+            case "d":
+                audio = new Audio("./sounds/tom-4.mp3");
+                break;
+            case "j":
+                audio = new Audio("./sounds/snare.mp3");
+                break;
+            case "k":
+                audio = new Audio("./sounds/crash.mp3");
+                break;
+            case "l":
+                audio = new Audio("./sounds/kick-bass.mp3");
+                break;
+            default:
+                console.log("No key pressed.")
+        }
+
+        audio.play();
+    });
+});
