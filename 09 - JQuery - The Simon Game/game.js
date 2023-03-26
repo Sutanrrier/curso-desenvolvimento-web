1bd2cc64abf99712f72e51e0610a9e517459fb50
Sutanrrier <pabloryanbr0@gmail.com>
2023-03-26 10:44:01 -0300

    Projeto 09 iniciado - The Simon Game

---
+++ 09 - JQuery - The Simon Game/game.js
@@ -0,0 +1,55 @@
+const buttonColors = ["red", "blue", "green", "yellow"];
+
+const gamePattern = [];
+const playerPattern = [];
+
+nextSequence();
+
+//Lógica do jogador ao clicar em um botão
+$(".btn").on("click", function () {
+    const playerChosenColor = this.id;
+
+    playerPattern.push(playerChosenColor);
+    playButtonSound(playerChosenColor);
+    playButtonAnimation(playerChosenColor);
+});
+
+//Seleciona uma nova cor de forma aleatória e adiciona ao gamePattern atual
+function nextSequence() {
+    const randomChosenColor = buttonColors[Math.floor(Math.random() * 4)];
+
+    gamePattern.push(randomChosenColor);
+    playButtonSound(randomChosenColor);
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+}
+
+//Lógica para o som das teclas
+function playButtonSound(cor) {
+    let audio;
+
+    switch (cor) {
+        case "red":
+            audio = new Audio("./sounds/red.mp3");
+            break;
+        case "blue":
+            audio = new Audio("./sounds/blue.mp3");
+            break;
+        case "green":
+            audio = new Audio("./sounds/green.mp3");
+            break;
+        case "yellow":
+            audio = new Audio("./sounds/yellow.mp3");
+            break;
+    }
+
+    audio.play();
+}
+
+//Lógica para a animação das teclas
+function playButtonAnimation(cor) {
+    $("#" + cor).addClass("pressed");
+    setTimeout(() => {
+        $("#" + cor).removeClass("pressed");
+    }, 100)
+
+}
